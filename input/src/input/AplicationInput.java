9751074ac491b6c3d93f6f7d51d8f893d8c138fd
Rafal <bokstur.lodz@gmail.com>
2020-02-20 18:46:05 +0100

    zmiany w projekcie input

--- input/src/input/AplicationInput.java
+++ input/src/input/AplicationInput.java
@@ -1,20 +1,29 @@
 package input;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+
 public class AplicationInput {
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
-		
-		/*System.out.println("podaj imiê: ");
-		
-		String text = input.nextLine();
-		
-		System.out.println("zajebiste imiê: " + text);*/
-	
+
+		/*
+		 * System.out.println("podaj imiê: ");
+		 * 
+		 * String text = input.nextLine();
+		 * 
+		 * System.out.println("zajebiste imiê: " + text);
+		 */
+
 		System.out.println("insert a nr: ");
-		
-		double aValue = input.nextDouble();
-		
-		System.out.println("your nr is: " + aValue);
+		try {
+			double aValue = input.nextDouble();
+
+			System.out.println("your nr is: "  + aValue);
+		} catch (InputMismatchException e) {
+			System.out.println("nie prawid³owy format danych");
+		}
 	}
 }
