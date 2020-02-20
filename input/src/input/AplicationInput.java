462de527601d7fe1a63836e270a6bdd259f8d69f
Rafal <bokstur.lodz@gmail.com>
2020-02-20 19:06:57 +0100

    kurwa tyle się udało zrobić..w tym hałasie

--- input/src/input/AplicationInput.java
+++ input/src/input/AplicationInput.java
@@ -3,27 +3,32 @@ package input;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
 public class AplicationInput {
 	public static void main(String[] args) {
 
-		Scanner input = new Scanner(System.in);
-
+		// Scanner input = new Scanner(System.in);
+		// the previous lecture
 		/*
-		 * System.out.println("podaj imiê: ");
-		 * 
-		 * String text = input.nextLine();
-		 * 
+		 * System.out.println("podaj imiê: "); String text = input.nextLine();
 		 * System.out.println("zajebiste imiê: " + text);
 		 */
 
-		System.out.println("insert a nr: ");
-		try {
-			double aValue = input.nextDouble();
+		/*
+		 * System.out.println("insert a nr: "); try { double aValue =
+		 * input.nextDouble();
+		 * 
+		 * System.out.println("your nr is: " + aValue); } catch (InputMismatchException
+		 * e) { System.out.println("nie prawid³owy format danych"); }
+		 */
 
-			System.out.println("your nr is: "  + aValue);
-		} catch (InputMismatchException e) {
-			System.out.println("nie prawid³owy format danych");
+		Scanner input = new Scanner(System.in);
+		int number = 0;
+		do {
+			System.out.println("insert a number: ");
+			number = input.nextInt();
+		} while (number != 9);
+		{
+			System.out.println("you got 9");
 		}
 	}
 }
